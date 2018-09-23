a033be84fc8022d5f3c6d3e9aa7945b2b112183e
Skuznetz <s.kuznetzov@mail.ru>
2018-09-23 17:20:10 +0400

    напишем компонент Serch

--- src/containers/Search.jsx
+++ src/containers/Search.jsx
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import SearchBox from '../components/SearchBox.jsx';
 
-const Search = () => <h1>Search Page</h1>;
+export default class Search extends Component {
+    render() {
+        return (
+            <div>
+                <SearchBox />
+            </div>
+        );
+    }
+}
 
 export default Search;
 
