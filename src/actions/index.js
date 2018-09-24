8bb406a3eff9e2475975c9bc6ea86efca75401c7
Skuznetz <s.kuznetzov@mail.ru>
2018-09-24 17:55:49 +0400

    добавим экшены

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,21 @@
+export const FETCH_MOVIES_REQUEST = 'FETCH_MOVIES_REQUEST';
+export const FETCH_MOVIES_SUCCESS = 'FETCH_MOVIES_SUCCESS';
+export const FETCH_MOVIES_FAILURE = 'FETCH_MOVIES_FAILURE';
 
+export const fetchMoviesRequest = query => ({
+    type: FETCH_MOVIES_REQUEST,
+    query
+});
+
+export const fetchMoviesSuccess = ({ data }) => ({
+    ...data,
+    type: FETCH_MOVIES_SUCCESS
+});
+
+export const searchMovies = query => dispatch => {
+    dispatch(fetchMoviesRequest(query));
+
+    return api.searchMovies(query)
+        .then(data => dispatch(fetchMoviesSuccess(data)));
+}
 
