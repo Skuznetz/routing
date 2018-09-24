85cc35ef722c3b787cab96e29939f141eec5dfe3
Skuznetz <s.kuznetzov@mail.ru>
2018-09-24 17:41:41 +0400

    поднимем экспорт

--- src/containers/Search.jsx
+++ src/containers/Search.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import SearchBox from '../components/SearchBox.jsx';
 import MovieGrid from '../components/MovieGrid.jsx';
 
- class Search extends Component {
+export default class Search extends Component {
     render() {
         return (
             <div>
@@ -13,6 +13,6 @@ import MovieGrid from '../components/MovieGrid.jsx';
     }
 }
 
-export default Search;
+
 
 
