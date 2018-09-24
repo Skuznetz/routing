724c20421c33a5d63140e4dfe2a53d38cc90ae62
Skuznetz <s.kuznetzov@mail.ru>
2018-09-24 17:43:39 +0400

    импортируем connect

--- src/containers/Search.jsx
+++ src/containers/Search.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import SearchBox from '../components/SearchBox.jsx';
 import MovieGrid from '../components/MovieGrid.jsx';
+
 
 export default class Search extends Component {
     render() {
