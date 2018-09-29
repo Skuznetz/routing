6bd7b904c0fb19e41439d5491c080fe984c85ae1
Skuznetz <s.kuznetzov@mail.ru>
2018-09-29 22:29:48 +0400

    раскомментируем код

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,22 +5,22 @@ export const FETCH_MOVIES_REQUEST = 'FETCH_MOVIES_REQUEST';
 export const FETCH_MOVIES_SUCCESS = 'FETCH_MOVIES_SUCCESS';
 export const FETCH_MOVIES_FAILURE = 'FETCH_MOVIES_FAILURE';
 
-// export const fetchMoviesRequest = query => ({
-//     type: FETCH_MOVIES_REQUEST,
-//     query
-// });
+export const fetchMoviesRequest = query => ({
+    type: FETCH_MOVIES_REQUEST,
+    query
+});
 
-// export const fetchMoviesSuccess = ({ data }) => ({
-//     ...data,
-//     type: FETCH_MOVIES_SUCCESS
-// });
+export const fetchMoviesSuccess = ({ data }) => ({
+    ...data,
+    type: FETCH_MOVIES_SUCCESS
+});
 
-// export const searchMovies = query => dispatch => {
-//     dispatch(fetchMoviesRequest(query));
+export const searchMovies = query => dispatch => {
+    dispatch(fetchMoviesRequest(query));
 
-//     return api.searchMovies(query)
-//         .then(data => dispatch(fetchMoviesSuccess(data)));
-// }
+    return api.searchMovies(query)
+        .then(data => dispatch(fetchMoviesSuccess(data)));
+}
 
 // export const fetchMoreMovies = ({ query, page }) => dispatch => {
 //     dispatch(fetchMoviesRequest(query));
