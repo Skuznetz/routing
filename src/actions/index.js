51f5b581cac6d93cb36d77346cef3803b1f26f67
Skuznetz <s.kuznetzov@mail.ru>
2018-09-30 11:48:41 +0400

    изменим код

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,5 @@
 import api from '../api';
 
-
 export const FETCH_MOVIES_REQUEST = 'FETCH_MOVIES_REQUEST';
 export const FETCH_MOVIES_SUCCESS = 'FETCH_MOVIES_SUCCESS';
 export const FETCH_MOVIES_FAILURE = 'FETCH_MOVIES_FAILURE';
@@ -19,28 +18,6 @@ export const fetchMoviesSuccess = ({ data }) => ({
     type: FETCH_MOVIES_SUCCESS
 });
 
-export const searchMovies = query => dispatch => {
-    dispatch(fetchMoviesRequest(query));
-
-    return api.searchMovies(query)
-        .then(data => dispatch(fetchMoviesSuccess(data)));
-}
-
-// export const fetchMoreMovies = ({ query, page }) => dispatch => {
-//     dispatch(fetchMoviesRequest(query));
-
-//     return api.searchMovies(query)
-//         .then(data => dispatch(fetchMoviesSuccess(data)));
-// }
-
-
-export const fetchMovie = id => dispatch => {
-    dispatch(fetchMovieRequest(id));
-
-    return api.fetchMovie(id)
-        .then(data => dispatch(fetchMovieSuccess(data)));
-}
-
 export const fetchMovieRequest = id => ({
     type: FETCH_MOVIE_REQUEST,
     id
@@ -50,6 +27,15 @@ export const fetchMovieSuccess = ({ data }) => ({
     movie: data,
     type: FETCH_MOVIE_SUCCESS
 });
+
+export const searchMovies = query => dispatch => {
+    dispatch(fetchMoviesRequest(query));
+
+    return api.searchMovies(query)
+        .then(data => dispatch(fetchMoviesSuccess(data)));
+}
+
+
 
 export const fetchMovie = id => dispatch => {
     dispatch(fetchMovieRequest(id));
