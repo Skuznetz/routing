a343bed66c0a061a4bc605349edf29b4eb26d183
Skuznetz <s.kuznetzov@mail.ru>
2018-10-02 20:53:47 +0400

    напишем render

--- src/containers/Movie.jsx
+++ src/containers/Movie.jsx
@@ -6,12 +6,26 @@ import { fetchMovie } from '../actions';
     mapStateToProps,{fetchMovie}
 )
 export default class Movie extends Component {
-      componentDidMount() {
+    componentDidMount() {
         const { movieId,fetchMovie } = this.props;
 
         this.props.fetchMovie(movieId);
     }
+    render() {
+        const { movie, loading, children } = this.props;
+
+        return (
+            <Loader loading={loading}>
+                <BackdropContainer backdropPath={movie.backdropPath}>
+                    <MovieInfo {...movie} />
+                </BackdropContainer>
+
+                {children}
+            </Loader>
+        );
+    }
 }
+
 function maoStateToProps(state,ownProps){
     return {
         movie: state.movie.info,
