dc1409a9f0b64d0fceebc317db6c65bd30607bb6
Skuznetz <s.kuznetzov@mail.ru>
2018-10-02 20:51:01 +0400

    создадим класс Movie

--- src/containers/Movie.jsx
+++ src/containers/Movie.jsx
@@ -5,7 +5,13 @@ import { fetchMovie } from '../actions';
 @connect(
     mapStateToProps,{fetchMovie}
 )
+export default class Movie extends Component {
+      componentDidMount() {
+        const { movieId,fetchMovie } = this.props;
 
+        this.props.fetchMovie(movieId);
+    }
+}
 function maoStateToProps(state,ownProps){
     return {
         movie: state.movie.info,
