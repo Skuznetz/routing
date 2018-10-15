5c9ec9ad08396f0ed026f9bdc844a94cee2565d4
Skuznetz <s.kuznetzov@mail.ru>
2018-10-15 20:43:11 +0400

    напишем логику проверки

--- src/hoc/requireAuth.jsx
+++ src/hoc/requireAuth.jsx
@@ -7,3 +7,40 @@ const mapStateToProps = state => ({
     pathname: state.routing.locationBeforeTransitions.pathname,
     query: state.routing.locationBeforeTransitions.query,
 });
+
+export default function requireAuth(Component) {
+    @connect(mapStateToProps, { replace })
+    class AuthentificatedComponent extends React.Component {
+        componentWillMount() {
+            this.checkAuth(this.props);
+        }
+
+        componentWillReceiveProps(nextProps) {
+            this.checkAuth(nextProps);
+        }
+
+        checkAuth = props => {
+            if (!props.isLoggedIn) {
+                props.replace({
+                    pathname: '/login',
+                    state: {
+                        nextLocation: {
+                            pathname: props.pathname,
+                            query: props.query,
+                        }
+                    }
+                });
+            }
+        }
+        render(){
+            
+            const { isLoggedIn, ...otherProps } = this.props;
+
+            return isLoggedIn
+                ? <Component {...otherProps} />
+                : null;
+        }
+    }
+
+    return AuthentificatedComponent;
+}
