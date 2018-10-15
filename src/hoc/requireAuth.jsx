fa0c04fe5872564bd32f249829153b4d53c4bda7
Skuznetz <s.kuznetzov@mail.ru>
2018-10-15 20:37:34 +0400

    mapStateToProps

--- src/hoc/requireAuth.jsx
+++ src/hoc/requireAuth.jsx
@@ -1,3 +1,9 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { replace } from 'react-router-redux';
+
+const mapStateToProps = state => ({
+    isLoggedIn: isLoggedIn(state),
+    pathname: state.routing.locationBeforeTransitions.pathname,
+    query: state.routing.locationBeforeTransitions.query,
+});
