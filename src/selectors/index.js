4cda2d7156cf09de294e75c5c9db21589347018c
Skuznetz <s.kuznetzov@mail.ru>
2018-10-24 19:14:30 +0400

    напишем селекторы

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,1 +1,13 @@
+export const isLoggedIn = state => state.isLoggedIn;
 export const isLoggingIn = state => state.session.isLoggingIn;
+export const getAuthError = state => state.session.error;
+
+export const getLocation = state => state.routing.locationBeforeTransitions;
+
+export const isMovieFetching = state => state.movie.isFetching;
+export const getMovieInfo = state => state.movie.info;
+
+export const isMoviesFetching = state => state.movies.isFetching;
+export const getSearchMovies = state => state.movies.items;
+
+
